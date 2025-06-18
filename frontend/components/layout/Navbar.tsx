178d98708d1795ed13ecf63e68301b8a551efa87
yusufproject01 <yusufproject01@gmail.com>
2025-06-18 23:36:25 +0700

    FE-Navbar

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
-        <main className="w-full h-16 border-b border-slate-300 fixed z-20">
-            <h1>Navbar</h1>
+        <main className="w-full h-16 border-b px-12 border-slate-300 fixed z-20 bg-white flex items-center justify-between">
+            <h1>Brand</h1>
+                <ul className="flex gap-4 px-2">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/">Home</Link></li>
+                </ul>
         </main>
     )
 }
